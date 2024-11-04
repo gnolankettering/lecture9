ab467ac09199a8fd117f21bdb76e60aea473bebb
Godfrey Nolan <129680080+gnolankettering@users.noreply.github.com>
2024-11-04 16:55:53 -0500

    Create spotify_test.py

---
+++ spotify_test.py
@@ -0,0 +1,13 @@
+import spotipy
+import config
+
+sp = spotipy.Spotify(
+    auth_manager=spotipy.SpotifyOAuth(
+        client_id = config.SPOTIFY_CLIENT_ID,
+        client_secret = config.SPOTIFY_CLIENT_SECRET,
+        redirect_uri="http://127.0.0.1:8080"
+    )
+)
+
+current_user = sp.current_user()
+print(current_user)
